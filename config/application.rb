37e50671c63b90a9ae2b946b5029b32996b2b359
Ajeey <ajey.charantimath@gmail.com>
2014-02-02 21:57:06 +0530

    assests precompile false

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module Askrg
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     # google analytics
     config.middleware.use("Rack::GoogleAnalytics", :web_property_id => "UA-0000000-1")
   end
